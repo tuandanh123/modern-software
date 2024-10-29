fdea34f14b2951e235517ba7073852b8fcb506b2
tuandanh <nguyentuandanhk15@siu.edu.vn>
2024-10-29 11:10:12 +0700

    second commit

---
+++ src/main/java/com/example/modernsoftware/ModernsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.modernsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernsoftwareApplication.class, args);
+	}
+
+}
